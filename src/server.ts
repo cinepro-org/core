670358d25bb5bbd7c073e233fdda63414fb48398
FUSIONOF <jayeshanimeparmar@gmail.com>
2026-04-30 05:49:46 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -61,12 +61,12 @@ async function main() {
                 {
                     id: 'WebStreamerMBG',
                     url: 'https://87d6a6ef6b58-webstreamrmbg-dev.baby-beamup.club/manifest.json',
-                    enabled: true
+                    enabled: false
                 },
                 {
                     id: 'Streamify',
                     url: 'https://stremify.hayd.uk/manifest.json',
-                    enabled: true
+                    enabled: false
                 }
             ]
         },
